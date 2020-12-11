6f8515f0eae2de5990dc02575d82d405df3c431a
Giuseppe Cardillo <37176275+dnafinder@users.noreply.github.com>
2020-12-11 22:26:37 +0100

    Update regpolygon.m

--- regpolygon.m
+++ regpolygon.m
@@ -11,7 +11,7 @@ function regpolygon(varargin)
 %     Inputs:
 %           n: sides number, a scalar >=3 (3 by default)
 %           L: side length, a scalar >0 (1 by default) 
-%                 1 (right tailed)
+%
 %     Outputs:
 %           Sides
 %           Length
