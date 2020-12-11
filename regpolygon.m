07205c75ab39338619d124911ec1970dab4d7fde
Giuseppe Cardillo <37176275+dnafinder@users.noreply.github.com>
2020-12-11 22:00:20 +0100

    Update regpolygon.m

--- regpolygon.m
+++ regpolygon.m
@@ -1,4 +1,67 @@
 function regpolygon(varargin)
+%In Euclidean geometry, a regular polygon is a polygon that is equiangular
+%(all angles are equal in measure) and equilateral (all sides have the same
+%length). This function gives all the possible properties of a regulare
+%polygon. Moreover, for polygons with 3<=n<=12 sides, will be shown an
+%animation to how contruct it (exactly or approximately) using ruler and
+%compass. 
+% 
+% Syntax: 	regpolygon(varargin)
+%    
+%     Inputs:
+%           n: sides number, a scalar >=3 (3 by default)
+%           L: side length, a scalar >0 (1 by default) 
+%                 1 (right tailed)
+%     Outputs:
+%           Sides
+%           Length
+%           Fixed number
+%           Apotema
+%           Inscribed circle area
+%           Height
+%           number of diagonals
+%           Perimeter
+%           Area fixed number (phi)
+%           Area
+%           Circumradius
+%           Circumscribed circle area
+%           Interior angle 
+%           Exterior angle 
+%           Constructible or not 
+%
+%      Example: 
+%
+%           Calling on Matlab the function: regpolygon(5)
+%
+%           Answer is:
+%
+%                                   Value 
+%                                  _______
+% 
+%     Sides                              5
+%     Length                             1
+%     Fixed_Number_(f)             0.68819
+%     Apotema_(green)              0.68819
+%     Inscribed_circle_area         1.4879
+%     Height                        1.5388
+%     n_of_Diagonals                     5
+%     Perimeter                          5
+%     Area_fixed_number_(phi)       1.7205
+%     Area                          1.7205
+%     Circumradius_(blue)          0.85065
+%     Circumscribed_circle_area     2.2733
+% 
+% Interior angle: 3/5*pi	108.00°
+% Exterior angle: 2/5*pi	72.00°
+% Constructible polygon
+%
+%           Created by Giuseppe Cardillo
+%           giuseppe.cardillo.75@gmail.com
+%
+% To cite this file, this would be an appropriate format:
+% Cardillo G. (2020). Regular polygons: facts and drawing
+% https://github.com/dnafinder/regpolygon
+
 p = inputParser;
 addOptional(p,'n',3,@(x) validateattributes(x,{'numeric'},{'scalar','real','finite','nonnan','nonempty','integer','nonzero','>=',3}));
 addOptional(p,'L',1,@(x) validateattributes(x,{'numeric'},{'scalar','real','finite','nonnan','nonempty','integer','nonzero','>',0}));
@@ -8,14 +71,15 @@ clear p varargin
 
 close all
 clc
-fn=@(n) 1/2/tan(pi/n);
+fn=@(n) 1/2*cot(pi/n);
 Apotema=@(n,L) L*fn(n);
 Perimeter=@(n,L) n*L;
 phi=@(n) n/2*fn(n);
 Area=@(n,L) L^2*phi(n);
 circle=@(r) pi*r^2;
 a=Apotema(n,L);
-circumradius=sqrt(a^2+L/2^2);
+cr=@(n,L) L/2*csc(pi/n);
+circumradius=cr(n,L);
 iangle=@(n)(n-2)/n;
 eangle=@(n) 2/n;
 ndiags=@(n) 1/2*n*(n-3);
@@ -25,8 +89,13 @@ else
     h=a+circumradius;
 end
 disp(array2table([n;L;fn(n);a;circle(a);h;ndiags(n);Perimeter(n,L);phi(n);Area(n,L);circumradius;circle(circumradius)],'RowNames',{'Sides','Length','Fixed_Number_(f)','Apotema_(green)','Inscribed_circle_area','Height','n_of_Diagonals','Perimeter','Area_fixed_number_(phi)','Area','Circumradius_(blue)','Circumscribed_circle_area'},'VariableNames',{'Value'}))
-fprintf('Interior angle (*pi): %s\n',rats(iangle(n)));
-fprintf('Exterior angle (*pi): %s\n',rats(eangle(n)));
+fprintf('Interior angle: %s*pi\t%0.2f°\n',strtrim(rats(iangle(n))),iangle(n)*180);
+fprintf('Exterior angle: %s*pi\t%0.2f°\n',strtrim(rats(eangle(n))),eangle(n)*180);
+if all(ismember(factor(n),[2 3 5 17 257 65537]))
+    disp('Constructible polygon');
+else
+    disp('Not constructible polygon');
+end
 clear fn Apotema Perimeter Area circle
 
 if ismember(n,3:1:12)
